55eb8580fc053162db3efb67f50d4e6b364550b3
goriiin <afk00.17@bk.ru>
2023-08-12 00:21:55 +0300

    add router

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.20
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.25.0 // indirect
 )
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,25 @@
 package apiserver
 
 import (
+	"github.com/goriiin/dp-serv/internal/store"
+	"github.com/gorilla/mux"
 	"go.uber.org/zap"
+	"io"
+	"net/http"
 )
 
 type APIServer struct {
 	config *Config
 	logger *zap.Logger
+	router *mux.Router
+	store  *store.Store
 }
 
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: zap.NewExample(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -21,8 +28,14 @@ func (s *APIServer) Start() error {
 		return err
 	}
 
+	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
 	s.logger.Info("starting API server")
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
@@ -33,4 +46,25 @@ func (s *APIServer) configureLogger() error {
 
 	s.logger.Level()
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/", s.HandleTest())
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+
+	return nil
+}
+
+func (s *APIServer) HandleTest() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "TEST")
+	}
 }
