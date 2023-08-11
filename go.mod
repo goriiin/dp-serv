1657dfdfd71f20480819884990d38960f282e0a4
goriiin <afk00.17@bk.ru>
2023-08-11 22:07:13 +0300

    add logger pack logger - zap

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/goriiin/dp-serv
 
 go 1.20
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.25.0 // indirect
+)
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,15 +1,36 @@
 package apiserver
 
+import (
+	"go.uber.org/zap"
+)
+
 type APIServer struct {
 	config *Config
+	logger *zap.Logger
 }
 
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: zap.NewExample(),
 	}
 }
 
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting API server")
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	_, err := zap.ParseAtomicLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.Level()
 	return nil
 }
